aed62230f680d95ac498e916c2ddb1543c289787
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2019-10-22 13:41:16 +0100

    Print stderr

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 echo "use Red <red-do>; red-defaults(default => database('SQLite', :database<red.db>)); use lib './samples'; " > ./code.p6
 echo $* >> ./code.p6
 
-perl6 ./code.p6
+perl6 ./code.p6 2>&1
